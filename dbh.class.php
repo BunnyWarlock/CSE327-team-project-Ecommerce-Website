20e8bb71646d293dae5fce95a680738983bc2194
TanvirAhmedCSE <141907076+TanvirAhmedCSE@users.noreply.github.com>
2023-10-29 18:28:03 +0600

    dbh.class.php

--- dbh.class.php
+++ dbh.class.php
@@ -1,12 +1,26 @@
 <?php
-
+/**
+ * Class Dbh
+ * This class provides a PDO database connection.
+ */
 class Dbh{
-  
+
+  /**
+ * The server details for the database connection.
+ * @var string $servername The server name.
+ * @var string $username The username to connect to the database.
+ * @var string $password The password to connect to the database.
+ * @var string $dbname The name of the database to connect to.
+ */
   private $host = "localhost";
   private $user = "root";
   private $password = "";
   private $dbname = "cse327project";
 
+  /**
+     * Establish a database connection using PDO.
+     * @return PDO|false A PDO database connection object or false if the connection fails.
+    */
   protected function connect() {
     $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
     $pdo = new PDO($dsn, $this->user, $this->password);
@@ -14,10 +28,3 @@ class Dbh{
     return $pdo;
   }
 }
-
-
-
-
-
-
-
