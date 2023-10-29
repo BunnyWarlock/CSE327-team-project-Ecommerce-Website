031fd03ac95a61a8c89f6db6ba092b86fbe5e338
TanvirAhmedCSE <141907076+TanvirAhmedCSE@users.noreply.github.com>
2023-10-29 18:22:06 +0600

    model.php

--- model.php
+++ model.php
@@ -1,11 +1,26 @@
 <?php
+/**
+ * Class CartModel
+ * This class provides methods for managing shopping cart items in a database.
+ */
 class CartModel {
+    /**
+     * @var mysqli The database connection.
+     */
     private $conn;
 
+    /**
+     * CartModel constructor.
+     * @param mysqli $conn The database connection to be used by the model.
+     */
     public function __construct($conn) {
         $this->conn = $conn;
     }
 
+    /**
+     * Get the list of items in the shopping cart.
+     * @return array An array containing the cart items.
+     */
     public function getCartItems() {
         $query = "SELECT * FROM 'product_second' ORDER BY id ASC";
         $result = mysqli_query($this->conn, $query);
@@ -20,8 +35,13 @@ class CartModel {
         return $cartItems;
     }
 
+    /**
+     * Delete a cart item based on its product name.
+     * @param string $productName The name of the product to delete from the cart.
+     * @return bool True if the item was successfully deleted, false otherwise.
+     */
     public function deleteCartItem($productName) {
-        $deleteQuery = "DELETE FROM 'product_second' WHERE description = '$productName'";
+        $deleteQuery = "DELETE FROM `product_second` WHERE description = '$productName'";
         return mysqli_query($this->conn, $deleteQuery);
     }
 }
